38d1e66166070d0ace56fe6291e8d89d715c7f57
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 14:15:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,9 @@ def take_a_number (deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "The is nobody waiting to be served!"
+  else
+    puts "Currently"  
